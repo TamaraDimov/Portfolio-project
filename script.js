882bc3592450e124bdd6412b6ff7e2c3a2089a37
TamaraDimov <tamemojsoska@gmail.com>
2022-12-19 14:49:09 +0100

    Added style and link the menu btn

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const toggleNavbar = () => {
-    document.querySelector(".coverPage").classList.toggle("show")
-}
+  document.querySelector(".coverPage").classList.toggle("show");
+};
 
-document.querySelector(".toggleNav").addEventListener("click", function(){
-    toggleNavbar()
-})
+document.querySelector(".toggleNav").addEventListener("click", function () {
+  toggleNavbar();
+});
 
-document.querySelector(".exit").addEventListener("click", function(){
-    toggleNavbar()
-})
+const menuBtn = document.querySelectorAll(".menuBtn");
+Array.from(nodeList);
+const menuArr = Array.from(menubtn);
+menuArr.forEach((element) => {
+  element.addEventListener("click", function () {
+    toggleNavbar();
+  });
+});
