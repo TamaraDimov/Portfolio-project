a6a52c0fdfee1878e1685d0e877505d8ac1beccb
mrprotocoll <lekanvgbg@gmail.com>
2022-12-19 14:57:56 +0100

    Adjust script and styling

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ document.querySelector(".toggleNav").addEventListener("click", function () {
   toggleNavbar();
 });
 
+document.querySelector(".exit").addEventListener("click", function () {
+    toggleNavbar();
+});
+
 const menuBtn = document.querySelectorAll(".menuBtn");
-Array.from(nodeList);
-const menuArr = Array.from(menubtn);
+const menuArr = Array.from(menuBtn);
 menuArr.forEach((element) => {
   element.addEventListener("click", function () {
     toggleNavbar();
