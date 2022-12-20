045249bc3f327573f1deffb0ba61a6efca34aef5
mrprotocoll <lekanvgbg@gmail.com>
2022-12-20 13:59:31 +0100

    Add portfolio data as array to javascript

--- script.js
+++ script.js
@@ -25,6 +25,53 @@ menuArr.forEach((element) => {
 
 // }
 
+const workData = [
+  {
+    name: "Tonic",
+    image: "img/d1.png",
+    stack: "Back End Dev",
+    year: "2015",
+    excerpt: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tools: ["html","CSS","JavaScript"],
+    live_link: "#",
+    source_link: "#"
+  },
+  {
+    name: "Multi-Post Stories",
+    image: "img/d2.png",
+    stack: "Full Stack Dev",
+    year: "2015",
+    excerpt: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tools: ["html","Ruby on Rails","CSS","JavaScript"],
+    live_link: "#",
+    source_link: "#"
+  },
+  {
+    name: "FACEBOOK 360",
+    image: "assets\images\Snapshoot Portfolio3.png",
+    stack:'Full Stack Dev',
+    year:'2015',
+    excerpt: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tools: ["html","Ruby on Rails","CSS","JavaScript"],
+    live_link: "#",
+    source_link: "#"
+  },
+  {
+    name: "Uber Navigation",
+    image: "img/d4.png",
+    stack: "Lead Developer",
+    year: "2018",
+    excerpt: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tools: ["html","Ruby on Rails","CSS","JavaScript"],
+    live_link: "#",
+    source_link: "#"
+  }
+]
+
 const toggleWorkCard = () => {
   document.querySelectorAll(".more").classList.toggle("");
 };
