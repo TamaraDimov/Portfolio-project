02bfd86394439eb75875fcf472e6bc998c087175
TamaraDimov <tamemojsoska@gmail.com>
2022-12-20 12:03:37 +0100

    style the popup window

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const toggleNavbar = () => {
-  document.querySelector('.coverPage').classList.toggle('show');
+  document.querySelector(".coverPage").classList.toggle("show");
 };
 
-document.querySelector('.toggleNav').addEventListener('click', () => {
+document.querySelector(".toggleNav").addEventListener("click", () => {
   toggleNavbar();
 });
 
-document.querySelector('.exit').addEventListener('click', () => {
+document.querySelector(".exit").addEventListener("click", () => {
   toggleNavbar();
 });
 
-const menuBtn = document.querySelectorAll('.menuBtn');
+const menuBtn = document.querySelectorAll(".menuBtn");
 const menuArr = Array.from(menuBtn);
 menuArr.forEach((element) => {
-  element.addEventListener('click', () => {
+  element.addEventListener("click", () => {
     toggleNavbar();
   });
+});
+
+// let cardName = {'Tonic','Multi-post Stories','Tonic','Multi-post Stories'}
+// let tonic = {
+//   Image:
+//   name: 'Tonic',
+
+// }
+
+const toggleWorkCard = () => {
+  document.querySelectorAll(".more").classList.toggle("");
+};
+
+document.querySelectorAll(".more").addEventListener("click", () => {
+  toggleNavbar();
+});
+
+document.querySelector(".icon").addEventListener("click", () => {
+  toggleNavbar();
 });
