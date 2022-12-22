c7978918303545366f19426881eb7d1622ec5e3b
TamaraDimov <tamemojsoska@gmail.com>
2022-12-22 10:38:04 +0100

    Check and fix linter error

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const toggleNavbar = () => {
-  document.querySelector(".coverPage").classList.toggle("show");
+  document.querySelector('.coverPage').classList.toggle('show');
 };
 
-document.querySelector(".toggleNav").addEventListener("click", () => {
+document.querySelector('.toggleNav').addEventListener('click', () => {
   toggleNavbar();
 });
 
-document.querySelector(".exit").addEventListener("click", () => {
+document.querySelector('.exit').addEventListener('click', () => {
   toggleNavbar();
 });
 
-const menuBtn = document.querySelectorAll(".menuBtn");
+const menuBtn = document.querySelectorAll('.menuBtn');
 const menuArr = Array.from(menuBtn);
 menuArr.forEach((element) => {
-  element.addEventListener("click", () => {
+  element.addEventListener('click', () => {
     toggleNavbar();
   });
 });
 
 // Tools template
 const toolsTemplate = (toolsData) => {
-  let tools = "";
+  let tools = '';
   toolsData.forEach((tool) => {
     tools += `
     <button class="link">${tool}</button>
@@ -31,14 +31,14 @@ const toolsTemplate = (toolsData) => {
 
 // portfolio template
 const workCard = (portfolioData) => {
-  let content = "";
+  let content = '';
   portfolioData.forEach((workData) => {
     content += `
   <div class="mainContainer1 mobile-cards">
   <div class="img">
     <img class="imgBG" src="${
-      workData.image
-    }" alt="Catchy look inspiring bionic forms"></img>
+  workData.image
+}" alt="Catchy look inspiring bionic forms"></img>
   </div>
   <div class="infoCard">
     <section class="info">
@@ -55,8 +55,8 @@ const workCard = (portfolioData) => {
     <section class="linkButtons">${toolsTemplate(workData.tools)}</section>
     <div class="seeMore">
       <button class="more portfolioButton" data-id="${
-        workData.id
-      }">See Project</button>
+  workData.id
+}">See Project</button>
     </div>
   </div>
   </div>`;
@@ -83,8 +83,8 @@ const modal = (workData) => `<section class="popup">
             </section>
             <div class="img">
               <img class="imgBG" src="${
-                workData.image
-              }" alt="Image from the article for Tanner Christesen"></img>
+  workData.image
+}" alt="Image from the article for Tanner Christesen"></img>
             </div>
             <div class="decription">
             <div class="decriptionLeft">
@@ -94,15 +94,15 @@ const modal = (workData) => `<section class="popup">
             </div>
             <div class="decriptionRight">
             <section class="linkButtons">${toolsTemplate(
-              workData.tools
-            )}</section>
+    workData.tools,
+  )}</section>
             <div class="seeMore btnPopupDiv">
               <a href="${
-                workData.live_link
-              }" class="more btnPopup">See live <img class="" src="img/live.png" alt="go live icon"></img></a>
+  workData.live_link
+}" class="more btnPopup">See live <img class="" src="img/live.png" alt="go live icon"></img></a>
               <a href="${
-                workData.source_link
-              }"class="more btnPopup">See Source <img class="" src="img/git.png" alt="go live icon"></img></a>
+  workData.source_link
+}"class="more btnPopup">See Source <img class="" src="img/git.png" alt="go live icon"></img></a>
             </div>
           </div>
         </div>
@@ -111,92 +111,92 @@ const modal = (workData) => `<section class="popup">
 </section>`;
 
 // WINDOWS ON LOAD
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   const workData = [
     {
-      id: "1",
-      name: "Tonic",
-      image: "img/d1.png",
-      stack: "Back End Dev",
-      year: "2015",
+      id: '1',
+      name: 'Tonic',
+      image: 'img/d1.png',
+      stack: 'Back End Dev',
+      year: '2015',
       excerpt:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      tools: ["html", "CSS", "JavaScript"],
-      live_link: "#",
-      source_link: "#",
+      tools: ['html', 'CSS', 'JavaScript'],
+      live_link: '#',
+      source_link: '#',
     },
     {
-      id: "2",
-      name: "Multi-Post Stories",
-      image: "img/d2.png",
-      stack: "Full Stack Dev",
-      year: "2015",
+      id: '2',
+      name: 'Multi-Post Stories',
+      image: 'img/d2.png',
+      stack: 'Full Stack Dev',
+      year: '2015',
       excerpt:
-        "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      tools: ["html", "Ruby on Rails", "CSS", "JavaScript"],
-      live_link: "#",
-      source_link: "#",
+      tools: ['html', 'Ruby on Rails', 'CSS', 'JavaScript'],
+      live_link: '#',
+      source_link: '#',
     },
     {
-      id: "3",
-      name: "FACEBOOK 360",
-      image: "img/d3.png",
-      stack: "Full Stack Dev",
-      year: "2015",
+      id: '3',
+      name: 'FACEBOOK 360',
+      image: 'img/d3.png',
+      stack: 'Full Stack Dev',
+      year: '2015',
       excerpt:
         "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      tools: ["html", "Ruby on Rails", "CSS", "JavaScript"],
-      live_link: "#",
-      source_link: "#",
+      tools: ['html', 'Ruby on Rails', 'CSS', 'JavaScript'],
+      live_link: '#',
+      source_link: '#',
     },
     {
-      id: "4",
-      name: "Uber Navigation",
-      image: "img/d4.png",
-      stack: "Lead Developer",
-      year: "2018",
+      id: '4',
+      name: 'Uber Navigation',
+      image: 'img/d4.png',
+      stack: 'Lead Developer',
+      year: '2018',
       excerpt:
-        "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+        'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      tools: ["html", "Ruby on Rails", "CSS", "JavaScript"],
-      live_link: "#",
-      source_link: "#",
+      tools: ['html', 'Ruby on Rails', 'CSS', 'JavaScript'],
+      live_link: '#',
+      source_link: '#',
     },
   ];
 
   // function to load portfolio sections dynamically when page loads
   function loadPortfolio(callBack = () => null) {
-    const workSectionElemnt = document.querySelector("#portfolio");
+    const workSectionElemnt = document.querySelector('#portfolio');
     workSectionElemnt.innerHTML = workCard(workData);
     return callBack();
   }
   // function to show modal when portfolio see more button is clicked
   function showModal(modalData, callBack = () => null) {
-    document.querySelector(".popupSection").innerHTML = modal(modalData);
+    document.querySelector('.popupSection').innerHTML = modal(modalData);
     return callBack();
   }
 
   // function to close modal when close button is clicked
   function closeModal() {
-    document.querySelector(".icon").addEventListener("click", () => {
-      document.querySelector(".popupSection").innerHTML = "";
+    document.querySelector('.icon').addEventListener('click', () => {
+      document.querySelector('.popupSection').innerHTML = '';
     });
   }
 
   // load portfolio section
   loadPortfolio(() => {
-    const portfolioButton = document.querySelectorAll(".portfolioButton");
+    const portfolioButton = document.querySelectorAll('.portfolioButton');
     Array.from(portfolioButton).forEach((element) => {
-      element.addEventListener("click", (e) => {
+      element.addEventListener('click', (e) => {
         e.preventDefault();
-        const id = element.getAttribute("data-id");
+        const id = element.getAttribute('data-id');
         const filter = workData.filter((data) => data.id === id);
 
         // show modal
@@ -205,51 +205,51 @@ window.addEventListener("load", () => {
     });
   });
   // contact form validation
-  const email = document.getElementById("email");
-  const name = document.getElementById("name");
-  const text = document.getElementById("text");
+  const email = document.getElementById('email');
+  const name = document.getElementById('name');
+  const text = document.getElementById('text');
 
-  document.querySelector("#get-in-touch").addEventListener("click", () => {
+  document.querySelector('#get-in-touch').addEventListener('click', (e) => {
     // if the email field is valid, we let the form submit
     if (email.validity.typeMismatch) {
       // submit form
-      document.getElementById("error").innerHTML = "Invalid Email";
+      document.getElementById('error').innerHTML = 'Invalid Email';
       e.preventDefault();
       return;
     }
     if (email.validity.patternMismatch) {
       // submit form
-      document.getElementById("error").innerHTML =
-        "Please type for email with lowercase letters";
+      document.getElementById('error').innerHTML = 'Please type for email with lowercase letters';
       e.preventDefault();
       return;
     }
 
     if (!name.validity.valid) {
       // submit form
-      document.getElementById("error").innerHTML = "Invalid Name";
+      document.getElementById('error').innerHTML = 'Invalid Name';
       e.preventDefault();
       return;
     }
 
     if (!text.validity.valid) {
       // submit form
-      document.getElementById("error").innerHTML = "Invalid Text";
+      document.getElementById('error').innerHTML = 'Invalid Text';
       e.preventDefault();
       return;
     }
 
-    const form = document.getElementById("contact-form");
+    const form = document.getElementById('contact-form');
     const myFormData = new FormData(form);
     const formDataObj = Object.fromEntries(myFormData.entries());
-    localStorage.setItem("contactform", JSON.stringify(formDataObj));
+    localStorage.setItem('contactform', JSON.stringify(formDataObj));
     // submit form
     document.getElementById('contact-form').submit();
-    
   });
 
-  const contactFormData = JSON.parse(localStorage.getItem("contactform") ?? '{}');
-  name.value = contactFormData.name ?? "";
-  email.value = contactFormData.email ?? "";
-  text.value = contactFormData.message ?? "";
+  const contactFormData = JSON.parse(
+    localStorage.getItem('contactform') ?? '{}',
+  );
+  name.value = contactFormData.name ?? '';
+  email.value = contactFormData.email ?? '';
+  text.value = contactFormData.message ?? '';
 });
